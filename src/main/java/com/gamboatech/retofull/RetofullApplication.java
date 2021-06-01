b49d54a3fdb780cdefb4312bdb9744e0339eba4f
Nixon Gamboa <nrgamboa97@gmail.com>
2021-06-01 18:52:28 -0500

    Proyecto spring inicializado con JPA, Web y h2

---
+++ src/main/java/com/gamboatech/retofull/RetofullApplication.java
@@ -0,0 +1,13 @@
+package com.gamboatech.retofull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetofullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetofullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamboatech/retofull/RetofullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamboatech.retofull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetofullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
