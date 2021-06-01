30a202dc4526f2a77781d2d12245e544b2dab161
Nixon Gamboa <nrgamboa97@gmail.com>
2021-06-01 18:55:30 -0500

    Clase ToDo agregada como modelo

---
+++ src/main/java/com/gamboatech/retofull/ToDo.java
@@ -0,0 +1,38 @@
+package com.gamboatech.retofull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class ToDo {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private boolean isCompleted;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+}
