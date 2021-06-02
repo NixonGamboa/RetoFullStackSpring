d2b27276953ce61b8106fe7ae48e20cfea4c754b
Nixon Gamboa <nrgamboa97@gmail.com>
2021-06-01 19:18:06 -0500

    ToDoController creado, API funcional y probada

---
+++ src/main/java/com/gamboatech/retofull/ToDoController.java
@@ -0,0 +1,37 @@
+package com.gamboatech.retofull;
+
+import com.gamboatech.retofull.repository.ToDoService;
+import com.gamboatech.retofull.repository.ToDoServiceImplement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.desktop.OpenFilesEvent;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api")
+public class ToDoController {
+
+    @Autowired
+    private ToDoService toDoService;
+
+
+
+    @GetMapping()
+    public List<ToDo> list() {
+         return toDoService.list();
+    }
+    @GetMapping("/{id}")
+    public Optional<ToDo> get(@PathVariable("id") long id){
+        return toDoService.get(id);
+    }
+    @PostMapping(value = "/save")
+    public ToDo save(@RequestBody ToDo toDo){
+        return toDoService.save(toDo);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable("id") long id){
+        toDoService.delete(id);
+    }
+}
